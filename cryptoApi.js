b8ed320d879d5b520aae6e3af4dbe8ce81d8da34
Umesh594 <146110753+Umesh594@users.noreply.github.com>
2025-06-29 00:00:27 +0530

    Update cryptoApi.js

--- cryptoApi.js
+++ cryptoApi.js
@@ -1,40 +1,30 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
-// Note: Change v1 to v2 on rapid api
-
-const cryptoApiHeaders = {
-  'x-rapidapi-host': process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
-  'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
-};
-const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
-
-export const cryptoApi = createApi({
-  reducerPath: 'cryptoApi',
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_CRYPTO_API_URL }),
-  endpoints: (builder) => ({
-    getCryptos: builder.query({
-      query: (count) => createRequest(`/coins?limit=${count}`),
-    }),
-
+const cryptoApiHeaders={
+    'x-rapidapi-key': '1ee70c29b0msh84625bfb960b42fp1e07dcjsn47bed956fed7',
+      'x-rapidapi-host': 'coinranking1.p.rapidapi.com'
+}
+  const baseUrl='https://coinranking1.p.rapidapi.com';
+  const createRequest =(url)=>({url,headers:cryptoApiHeaders});
+  export const cryptoApi=createApi({
+    reducerPath:'cryptoApi',
+    baseQuery:fetchBaseQuery({baseUrl}),
+      endpoints:(builder)=>({
+           getCryptos: builder.query({
+              query:(count)=>createRequest(`/coins?limit=${count}`),
+           }),
     getCryptoDetails: builder.query({
       query: (coinId) => createRequest(`/coin/${coinId}`),
     }),
-
-    // Note: Change the coin price history endpoint from this - `coin/${coinId}/history/${timeperiod} to this - `coin/${coinId}/history?timeperiod=${timeperiod}`
     getCryptoHistory: builder.query({
-      query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
-    }),
-
-    // Note: To access this endpoint you need premium plan
-    getExchanges: builder.query({
-      query: () => createRequest('/exchanges'),
-    }),
-  }),
+      query: ({ coinId, timePeriod }) => 
+        createRequest(`/coin/${coinId}/history?timePeriod=${timePeriod}`),
+    })
+  })
 });
 
-export const {
+export const { 
   useGetCryptosQuery,
-  useGetCryptoDetailsQuery,
   useGetExchangesQuery,
-  useGetCryptoHistoryQuery,
+  useGetCryptoDetailsQuery,
+  useGetCryptoHistoryQuery
 } = cryptoApi;
