a56fde9d80eb748f22e5d95ad2139ce170c47eca
Umesh594 <146110753+Umesh594@users.noreply.github.com>
2025-06-29 00:01:06 +0530

    Update cryptoNewsApi.js

--- cryptoNewsApi.js
+++ cryptoNewsApi.js
@@ -1,21 +1,22 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoNewsHeaders = {
-  'x-bingapis-sdk': 'true',
-  'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY,
-  'x-rapidapi-host': process.env.REACT_APP_NEWS_RAPIDAPI_HOST,
+  'x-rapidapi-key': '1ee70c29b0msh84625bfb960b42fp1e07dcjsn47bed956fed7',
+  'x-rapidapi-host': 'crypto-news16.p.rapidapi.com', 
 };
+
+const baseUrl = 'https://crypto-news16.p.rapidapi.com'; 
 
 const createRequest = (url) => ({ url, headers: cryptoNewsHeaders });
 
 export const cryptoNewsApi = createApi({
   reducerPath: 'cryptoNewsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_NEWS_API_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getCryptoNews: builder.query({
-      query: ({ newsCategory, count }) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
+      query: () => createRequest('/news/top/5'),
     }),
   }),
 });
 
-export const { useGetCryptoNewsQuery } = cryptoNewsApi;
+export const { useGetCryptoNewsQuery } = cryptoNewsApi; 
